afb9108a0491a9a058146d4f1b9d8f3f0f374339
taka sato <comfynode@outlook.jp>
2021-06-08 18:16:01 +0900

    2021/06/08 first commit.

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+        resolve: `gatsby-theme-blog`,
+        options: {
+          // basePath defaults to `/`
+          basePath: `/blog`,
+        },
+      },    
   ],
 }
