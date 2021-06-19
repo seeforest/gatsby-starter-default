a9a357dfe03bc6d967413d8d89f46dee06945ea3
taka sato <comfynode@outlook.jp>
2021-06-19 09:55:38 +0900

    「カスタマイズしたから、再アップロードしました。」バージョン。

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
+    <p>カスタマイズしたから、再アップロードしました。</p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
