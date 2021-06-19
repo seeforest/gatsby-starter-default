e0cb6e13b0088fcbc9b11c9b0229374d69e1f251
taka sato <comfynode@outlook.jp>
2021-06-19 13:06:25 +0900

    gatsby-theme-blog プラグインをアンインストールした版。

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,12 +32,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-        resolve: `gatsby-theme-blog`,
-        options: {
-          // basePath defaults to `/`
-          basePath: `/blog`,
-        },
-      },    
   ],
 }
