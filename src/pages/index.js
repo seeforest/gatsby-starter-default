70973e8f60cd6eb3dc455936928ae81c7ec5db22
taka sato <comfynode@outlook.jp>
2021-06-22 13:55:21 +0900

    Gatsby  cloud に設定した環境変数にアクセスし表示する機能を追加。 GATSBY_SHOPIFY_STORE_URL:release URL GATSBY_STOREFRONT_ACCESS_TOKEN:release access token

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,17 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+console.log(`${process.env.NODE_ENV}`);     // development
+console.log(`${process.env.GATSBY_SHOPIFY_STORE_URL}`);     // .env.development ファイルの GATSBY_SHOPIFY_STORE_URL キーの値 your-url.myshopify.com
+console.log(`${process.env.GATSBY_STOREFRONT_ACCESS_TOKEN}`);   // .env.development ファイルの GATSBY_STOREFRONT_ACCESS_TOKEN キーの値 XXX
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
+    <p>NODE_ENV is {`${process.env.NODE_ENV}`}</p>
+    <p>GATSBY_SHOPIFY_STORE_URL is {`${process.env.GATSBY_SHOPIFY_STORE_URL}`}</p>
+    <p>GATSBY_STOREFRONT_ACCESS_TOKEN is {`${process.env.GATSBY_STOREFRONT_ACCESS_TOKEN}`}</p>
     <p>Hoting環境をFirebaseに変更しました。</p>
     <p>カスタマイズしたから、再アップロードしました。</p>
     <p>Welcome to your new Gatsby site.</p>
